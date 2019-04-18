b65a9bb313858f4c4c9f909810dcf3d002353cb9
chris <christopherscaudill@gmail.com>
2019-04-18 01:32:31 -0400

    set

--- FourLetterWords/Program.cs
+++ FourLetterWords/Program.cs
@@ -10,3 +10,9 @@ namespace FourLetterWords
         }
     }
 }
+
+// Function takes a sentence and returns the number of four letter words it contains
+// Test inputs:
+// "This sentence is fine" - Output: 2
+// "So is this one" - Output: 1
+// "Hello" - 0
---
+++ FourLetterWords/UserSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourLetterWords
+{
+    class UserSolution
+    {
+        public int four_letter_words(string s)
+        {
+            int x = 0;
+
+            return x;
+        }
+    }
+}
